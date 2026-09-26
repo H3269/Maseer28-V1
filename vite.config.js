0a0f9d1843e8348c3a4b677a94adaf925de2e7cc
H3269 <has.ab3269@gmail.com>
2026-09-26 19:49:55 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Maseer28-V39-Test/',
+  base: '/Maseer28-V1/',
   plugins: [react()],
-  build: { target: 'es2020', sourcemap: false },
+  build: {
+    target: 'es2020',
+    sourcemap: false,
+  },
 });
